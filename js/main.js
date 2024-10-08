77d33c239d2eb268da273718e7e433d9a5db7c15
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-08 15:58:04 +0200

    added api call

--- js/main.js
+++ js/main.js
@@ -5,5 +5,23 @@ createApp({
         return {
 
         }
+    },
+
+    methods: {
+        getDisk() {
+            axios.get('http://localhost/php-dischi-json/api/dischi-api.php')
+                .then(function (response) {
+                    // handle success
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+        }
+    },
+
+    created() {
+        getDisk();
     }
 }).mount('#app')
