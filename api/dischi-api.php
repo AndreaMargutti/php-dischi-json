dd0be59122f013ee097ff9f2ba46247df1504ff2
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-08 15:46:24 +0200

    added data

---
+++ api/dischi-api.php
@@ -0,0 +1,10 @@
+<?php
+// # Recupero il file contenente i dati
+$dischi = file_get_contents("../data/dischi.json");
+// var_dump($dischi);
+
+// # Dico al browser di analizzarlo come file json
+header("Content-Type: application/json")
+
+
+?>
