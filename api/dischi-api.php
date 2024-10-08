3e46399c0fba4e25cb809cf9cd5fca6be2a94ed2
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-08 17:10:27 +0200

    better comments

--- api/dischi-api.php
+++ api/dischi-api.php
@@ -6,7 +6,6 @@ $dischi = file_get_contents("../data/dischi.json");
 // # Dico al browser di analizzarlo come file json
 header("Content-Type: application/json");
 
-//mostro i contenuti
+// # mostro i contenuti
 echo $dischi;
-
 ?>
--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
+// Created App with Vue
 const { createApp } = Vue
 
 createApp({
     data() {
         return {
+            //Created empty array for data
             diskList: [],
         }
     },
 
     methods: {
+        // API Call with axios
         getDisk() {
             axios.get('http://localhost/php-dischi-json/api/dischi-api.php')
                 .then(response => {
                     // handle success
                     console.log(response.data);
+                    //Add the data in the disklist array
                     this.diskList = response.data;
                     console.log(this.diskList);
                 })
@@ -20,14 +24,11 @@ createApp({
                     // handle error
                     console.log(error);
                 })
-        },
-
-        getImgPath() {
-
-        },
+        }
     },
 
     created() {
+        //Call the function at the creation of the app
         this.getDisk()
     }
 }).mount('#app')
