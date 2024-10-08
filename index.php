53e53804703f9f0c415721fea3c8a92472e6784d
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-08 15:24:49 +0200

    added stack

--- index.php
+++ index.php
@@ -7,11 +7,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Bootstraps-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!--General Style-->
+    <link rel="stylesheet" href="/style/general.css">
+    <!--Axios-->
+    <script defer src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!--Vue-->
+    <script defer src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!--Main-->
+    <script src="./js/main.js"></script>
     <title>PHP - Dischi JSON</title>
 </head>
 
 <body>
+    <div id="app">
 
+    </div>
 </body>
 
 </html>
